9c84fb2dca9999c0e16c8bb1c7a7dde2ac37ff1e
navdheep <naveencarrer@gmail.com>
2015-01-07 10:13:26 +0530

    new class added

---
+++ test/Package1/newclass.py
@@ -0,0 +1,20 @@
+'''
+Created on 07-Jan-2015
+
+@author: naveen
+'''
+Traceback (most recent call last):
+  File "C:\Program Files\Python2.7.6\lib\runpy.py", line 162, in _run_module_as_
+main
+    "__main__", fname, loader, pkg_name)
+  File "C:\Program Files\Python2.7.6\lib\runpy.py", line 72, in _run_code
+    exec code in run_globals
+  File "C:\Program Files\Python2.7.6\pip.exe\__main__.py", line 9, in <module>
+  File "C:\Program Files\Python2.7.6\lib\site-packages\pip\__init__.py", line 18
+5, in main
+    return command.main(cmd_args)
+  File "C:\Program Files\Python2.7.6\lib\site-packages\pip\basecommand.py", line
+ 161, in main
+    text = '\n'.join(complete_log)
+UnicodeDecodeError: 'ascii' codec can't decode byte 0xae in position 80: ordinal
+ not in range(128)
